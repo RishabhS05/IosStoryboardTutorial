bfeef9167317b343d338fb12f5b5317756103e11
RishabhS05 <rishabhshri2795@gmail.com>
2024-07-20 11:02:30 +0530

    Added viewcontroller lifecycle

--- ProjectWithStoryboard/ColorDetailsVC.swift
+++ ProjectWithStoryboard/ColorDetailsVC.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class ColorDetailsVC: UIViewController {
     var color : UIColor?
+ 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        print("init- lifecycle of ViewController")
+    }
+//    override func loadView() {
+//        print("0) - loadView()")
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = color ?? .blue
+        print("1) - viewDidLoad()")
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+       print("2) - viewWillAppear()")
+    }
+    override func viewIsAppearing(_ animated: Bool) {
+        print("3) - viewIsAppearing()")
+    }
+   
+    override func viewWillLayoutSubviews() {
+        print("4) - viewWillLayoutSubviews()")
+    }
+    override func viewDidLayoutSubviews() {
+    
+        print("5) - viewDidlayoutSubviews()")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("6) - viewDidAppear()")
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: any UIViewControllerTransitionCoordinator) {
+        print("7) - viewIsAppearing()")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        print("8) - viewWillDisappearing()")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        print("9) - viewDidDisappear()")
+    }
+    deinit{
+        print("deinit")
+    }
 }
