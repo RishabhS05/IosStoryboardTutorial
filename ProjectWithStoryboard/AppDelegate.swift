e6afe14dedc9b2b7f1f2c7425e30dee05ea45661
RishabhS05 <rishabhshri2795@gmail.com>
2024-07-27 04:42:39 +0530

    AppDelegate Life cycle.

--- ProjectWithStoryboard/AppDelegate.swift
+++ ProjectWithStoryboard/AppDelegate.swift
@@ -12,23 +12,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("AppDelegate : application - Called when the app first launched.")
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        print("AppDelegate : application(UIApplication,configurationForConnecting :UISceneSession,ConnectionOptions) -> UISceneConfigration \n \t\t- Called when a new scene session is being created. \n\t\t- Use this method to select a configration to create a new scene with")
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        print("AppDelegate : application(UIApplication, didDiscardSceneSession: Set<UISceneSession>) \n\t\t- Called when the user discards a scene session. \n\t\t- use this menthod to release any resource that were specific to the discarded scene, as they will not return")
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("AppDelegate : applicationWillEnterForground()\n\t\t- UIkit will not call it.\n -Called when the app is about become active from background.")
+    }
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("AppDelegate : applicationDidBecomeActive() \n\t\t- UIkit will not call it.\n -Called when the app is in foreground")
+    }
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("AppDelegate : applicationWillResignActive() \n\t\t- UIkit will not call it.-\n-Called when the app is going to enter background")
+    }
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("AppDelegate : applicationwillTerminate() \n\t\t- Called when the app is in foreground.")
+    }
 }
 
--- ProjectWithStoryboard/basics/readmeSwiftBasics.swift
+++ ProjectWithStoryboard/basics/readmeSwiftBasics.swift
@@ -4,5 +4,97 @@
 //
 //  Created by Rishabh Shrivastava on 21/07/24.
 //
-
 import Foundation
+// struct vs class
+
+/**
+ Structures
+ - struct is value type.
+ - structures comfoms to protocol provides std  functionality.
+ - Inheritance is not allowed with other structure, class and enum.
+ */
+struct User {
+    let name : String
+    let age : Int
+}
+
+/**
+ Classes
+ - classes are refrence type
+ - inherit other class and protocols.
+ */
+class Base {
+    let error : String?
+    let message : String?
+    init(error: String?, message: String?) {
+        self.error = error
+        self.message = message
+    }
+}
+
+class Response : Base {
+    override init(error: String?, message: String?) {
+        super.init(error: error, message: message)
+    }
+}
+
+/**
+ Protocols
+ - contains declarations of methods
+ - also contains variable with getter/setters
+ - can be inherited by class, structure and enums
+ - extended by other protocols
+ */
+protocol PhonePro{
+    var name : String { get set }
+    var company : String  { get set }
+    // init cannot have bodies
+    init()
+    init(name :String , company : String)
+  
+    func calculateChargingTime(batteryPower  : Int)-> Double
+    
+}
+//if want to have the definition of the function use extension of it
+extension PhonePro {
+    func calculateProcessingThreads(coreCount : Int ) -> Double {
+        return Double(coreCount) * 5.5
+    }
+}
+
+class Micromax : PhonePro{
+    var name: String
+    var company: String
+    required init() {
+        self.name = "name"
+        self.company = "company"
+       }
+    required init(name: String, company: String) {
+        self.company = company
+        self.name = name
+    }
+    
+    func calculateChargingTime(batteryPower: Int) -> Double {
+       return  Double(batteryPower) / 10
+    }
+}
+var micromax = Micromax(name: "micromax v1", company: "micromax")
+
+// Structure - protocol
+struct iPhone : PhonePro{
+    var name: String
+    var company: String
+    init() {
+        self.name = "name"
+        self.company = "company"
+       }
+     init(name: String, company: String) {
+        self.company = company
+        self.name = name
+    }
+    func calculateChargingTime(batteryPower: Int) -> Double {
+       return  Double(batteryPower) / 100
+    }
+}
+
+
